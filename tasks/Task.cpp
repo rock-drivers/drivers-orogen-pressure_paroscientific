a0783c6348f5d333bab4a1dab716acde05cdefcc
hbuettner <hbuettner@hbuettner-Precision-M4500.(none)>
2012-01-10 15:17:53 +0100

    initial commit

---
+++ tasks/Task.cpp
@@ -0,0 +1,85 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "Task.hpp"
+#include "pressure_paroscientific/Driver.hpp"
+
+using namespace pressure_paroscientific;
+
+Task::Task(std::string const& name)
+    : TaskBase(name),
+      mDriver(0)
+{
+}
+
+Task::Task(std::string const& name, RTT::ExecutionEngine* engine)
+    : TaskBase(name, engine)
+{
+}
+
+Task::~Task()
+{
+}
+
+
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See Task.hpp for more detailed
+// documentation about them.
+
+bool Task::configureHook()
+{
+    delete mDriver;
+    mDriver = new pressure_paroscientific::Driver;
+    if (!_io_port.get().empty())
+    {
+        mDriver->open(_io_port.get());
+    }
+
+    setDriver(mDriver);
+
+    if (! TaskBase::configureHook())
+        return false;
+    return true;
+}
+
+
+bool Task::startHook()
+{
+    if (! TaskBase::startHook())
+        return false;
+    return true;
+}
+
+
+void Task::processIO()
+{
+    mDriver->collectPeriodicData();
+
+    _paro_samples.write(mDriver->getData());
+
+}
+
+
+//void Task::updateHook()
+//{
+//    TaskBase::updateHook();
+//}
+
+
+// void Task::errorHook()
+// {
+//     TaskBase::errorHook();
+// }
+
+
+void Task::stopHook()
+{
+    TaskBase::stopHook();
+}
+
+
+// void Task::cleanupHook()
+// {
+//     TaskBase::cleanupHook();
+// }
+
